4fcdab657f4092eb77fc570da93f37dab851e4e7
shivamin27 <aminshiv01@gmail.com>
2017-02-14 20:51:23 -0700

    first commit

---
+++ src/First.java
@@ -0,0 +1,13 @@
+/**
+ * Created by shiv on 2/14/2017.
+ */
+public class First {
+
+
+    public static void main(String args[])
+    {
+        System.out.println("This is First Class");
+
+
+    }
+}
