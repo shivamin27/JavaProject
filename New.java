93bdfcd38de0dfa8b66a1e898f7a1118be6214a6
shivamin27 <aminshiv01@gmail.com>
2017-02-14 21:11:19 -0700

    Update New.java

--- New.java
+++ New.java
@@ -3,7 +3,7 @@ public class New {
 
     public static void main(String args[])
     {
-        System.out.println("This is NEW Class");
+        System.out.println("This is NEW Added Class");
 
 
     }
